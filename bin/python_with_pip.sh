c7fb2c829234b2905933b286feed5718e918919a
Amith Kadli Basavaraj <amith.kadlibasavaraj@boeing.com>
2019-07-30 17:35:54 +0530

    modification to script path

--- bin/python_with_pip.sh
+++ bin/python_with_pip.sh
@@ -4,17 +4,19 @@ build=$(cd "$1/" && pwd)
 cd $build
 wget https://www.python.org/ftp/python/3.4.5/Python-3.4.5.tgz
 tar zxfv Python-3.4.5.tgz
-find ~/filename/python -type d | xargs chmod 0755
+find $build/Python-3.4.5/Python -type d | xargs chmod 0755
 cd Python-3.4.5/
-./configure --prefix=$HOME/filename/python
+./configure --prefix=$build/Python-3.4.5/python
 make
 make install
-export PATH=$HOME/filename/python/Python-3.4.5/:$PATH
-
+export PATH=$build/Python-3.4.5/:$PATH
+echo '****Installing pip*******'
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py --user
 export PATH=$HOME/.local/bin/:$PATH
-python -V
 mkdir -p $build/.profile.d
-echo 'export PATH=$PATH:$HOME/filename/python/Python-3.4.5:$HOME/.local/bin' > $build/.profile.d/python.sh
-
+echo 'export PATH=$build/Python-3.4.5/:$HOME/.local/bin/:$PATH' > $build/.profile.d/python.sh
+echo 'Testing python'
+python -V
+pip -V
+ls -ltr
