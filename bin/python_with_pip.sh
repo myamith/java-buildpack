3c8a2490f82570bf5147c0a587c83bdc65c1c68f
Amith Kadli Basavaraj <amith.kadlibasavaraj@boeing.com>
2019-07-31 02:07:13 +0530

    update pip install in compilation

--- bin/python_with_pip.sh
+++ bin/python_with_pip.sh
@@ -13,14 +13,15 @@ make install
 export PATH=$build/python-all/Python-3.6.9:$PATH
 Python -V
 wget https://bootstrap.pypa.io/get-pip.py
-#python get-pip.py --user
-#export PATH=$HOME/.local/bin/:$PATH
+python get-pip.py --user
+export PATH=$HOME/.local/bin/:$PATH
+pip install -r $build/requirements.txt
 ls -ltr $build
 mkdir -p $build/.profile.d
 echo 'export PATH=/app/python-all/Python-3.6.9:/app/usr/local/bin:$PATH' > $build/.profile.d/python.sh
-echo 'python /app/python-all/Python-3.6.9/get-pip.py --user' >> $build/.profile.d/python.sh
-echo 'export PATH=$HOME/.local/bin/:$PATH' >> $build/.profile.d/python.sh
-echo 'mkdir -p /tmp/app/python-all/new_python/bin && ln -s /app/python-all/new_python/bin/python3.6 /tmp/app/python-all/new_python/bin/python3.6' >> $build/.profile.d/python.sh
+#echo 'python /app/python-all/Python-3.6.9/get-pip.py --user' >> $build/.profile.d/python.sh
+#echo 'export PATH=$HOME/.local/bin/:$PATH' >> $build/.profile.d/python.sh
+#echo 'mkdir -p /tmp/app/python-all/new_python/bin && ln -s /app/python-all/new_python/bin/python3.6 /tmp/app/python-all/new_python/bin/python3.6' >> $build/.profile.d/python.sh
 echo 'export PYTHONPATH="${PYTHONPATH}:/app/python-all/new_python/lib/python3.6/site-packages"' >> $build/.profile.d/python.sh
-echo 'pip install -r /app/requirements.txt' >> $build/.profile.d/python.sh
+#echo 'pip install -r /app/requirements.txt' >> $build/.profile.d/python.sh
 
