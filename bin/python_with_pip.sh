bbda58ee0a367afe5b5417e20f65dc127d57a934
Amith Kadli Basavaraj <amith.kadlibasavaraj@boeing.com>
2019-07-30 20:39:13 +0530

    update to new version of python

--- bin/python_with_pip.sh
+++ bin/python_with_pip.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 mkdir -p "$1"
-build=$(cd "$1/" && pwd)
-cd $build
-wget https://www.python.org/ftp/python/3.4.5/Python-3.4.5.tgz
-tar zxfv Python-3.4.5.tgz
-find $build/Python-3.4.5/Python -type d | xargs chmod 0755
-cd Python-3.4.5/
-./configure --prefix=$build/python
+build=$(cd "$1" && pwd)
+mkdir -p $build/python-all
+cd $build/python-all
+wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tgz
+tar zxfv Python-3.6.9.tgz
+find ./Python-3.6.9/Python -type d | xargs chmod 0755
+cd Python-3.6.9
+./configure --prefix $build/python-all/new_python
 make
 make install
-export PATH=$build/Python-3.4.5/:$PATH
+export PATH=$build/python-all/Python-3.6.9:$PATH
+Python -V
 echo '****Installing pip*******'
 wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --user
+$build/python-all/Python-3.6.9/python get-pip.py --user
 export PATH=$HOME/.local/bin/:$PATH
-mkdir -p $build/.profile.d
-echo 'export PATH=$build/Python-3.4.5/:$HOME/.local/bin/:$PATH' > $build/.profile.d/python.sh
 echo 'Testing python'
 python -V
 pip -V
 ls -ltr $build
-$build/python/bin/pip3 install -r $build/requirement.txt
+$build/python-all/new_python/bin/pip3 install -r $build/requirements.txt
